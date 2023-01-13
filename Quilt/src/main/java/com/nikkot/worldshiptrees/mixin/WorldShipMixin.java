f9601119e8ec368fae0b93282bad23c1b373d160
Nik-Kot <42574700+Nik-Kot@users.noreply.github.com>
2023-01-13 21:20:08 +0300

    Rename mod

--- Quilt/src/main/java/com/nikkot/worldshiptrees/mixin/WorldShipMixin.java
+++ Quilt/src/main/java/com/nikkot/worldshiptrees/mixin/WorldShipMixin.java
@@ -1,16 +1,16 @@
 package com.nikkot.worldshiptrees.mixin;
 
-import com.nikkot.worldshiptrees.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
+import com.nikkot.worldshiptrees.WorldShipTrees;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public class WorldShipMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		WorldShipTrees.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
